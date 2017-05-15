65427735d5465780c0703d1faabbcd89ad31438d
Brook <17863924158@163.com>
2017-05-15 13:21:53 +0800

    基类

---
+++ Basic.h
@@ -0,0 +1,17 @@
+#ifndef BASIC_H
+#define BASIC_H
+#include<string>
+#include<iostream>
+using namespace std;
+class Basic{
+protected:
+    string name,number,sex;
+    int age;
+public:
+    Basic()
+    {
+        //cin>>name>>number>>sex>>age;
+    }
+};
+#endif // BASIC_H
+
